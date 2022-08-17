bf151cec59b7b5782426a68cdba7807333bf8384
noobdev47 <sumairsaifofficial@gmail.com>
2022-08-17 09:55:22 +0500

    log added

--- web/frontend/components/providers/AppBridgeProvider.jsx
+++ web/frontend/components/providers/AppBridgeProvider.jsx
@@ -35,6 +35,7 @@ export function AppBridgeProvider({ children }) {
 	// Using state in this way is preferable to useMemo.
 	// See: https://stackoverflow.com/questions/60482318/version-of-usememo-for-caching-a-value-that-will-never-change
 	const [appBridgeConfig] = useState(() => {
+		console.log(import.meta.env.VITE_SHOPIFY_APP_URL)
 		return {
 			host: import.meta.env.VITE_SHOPIFY_APP_URL,
 			apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
